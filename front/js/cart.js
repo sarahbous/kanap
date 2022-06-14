bce93f7a9526312607faa52854a16d9de8bb80d1
Sarah Boussik <sarah.bouydarne@gmail.com>
2022-06-14 10:10:47 +0200

    Création de la page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,45 @@
-localStorage.setItem("_id", "colors", "quantite")
-console.log(localStorage)
+let addProduit = JSON.parse(localStorage.getItem("product"));
+
+//*********************************************** */
+
+
+
+
+//---------------------------------------------------//
+const panierDisplay = async () => {
+   console.log("salut");
+   if (addProduit) {
+      await addProduit;
+      console.log(addProduit);
+
+      document.getElementById("cartAndFormContainer").innerHTML = addProduit.map((product) => 
+    
+    `
+    <article class="cart__item" data-id="" data-color="">
+                <div class="cart__item__img">
+                  <img src=${product.imageUrl} alt="Photographie d'un canapé">
+   </img>             </div>
+               <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                   <h2>${product.name}</h2>
+                    <p>${product.teinte}</p>
+                    <p>${product.price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                   <div class="cart__item__content__settings__quantity">
+                     <p>Qté :  </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                   <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+     
+     ` )
+    };
+}; 
+   
+
+panierDisplay();
--- front/js/product.js
+++ front/js/product.js
@@ -7,8 +7,8 @@ console.log(urlSearchParams)
 let _id = urlSearchParams.get("id")
 console.log(_id)
 
-
 let productData = [];
+
 
 const fetchProducts = async () => {
     await fetch(`http://localhost:3000/api/products/${_id}`)
@@ -24,7 +24,7 @@ const productDisplay = async () => {
     document.title = `${productData.name}`;
     
     
-   document.getElementById("item").innerHTML  = 
+    document.getElementById("item").innerHTML  = 
    `
     
    <div class="item__img">
@@ -34,7 +34,9 @@ const productDisplay = async () => {
 
      <div class="item__content__titlePrice">
        <h1 id="title">${productData.name}</h1>
-       <p>Prix : <span id="price">${productData.price}</span>€</p>
+       <p>Prix : <span id="price">${productData.price
+      .toString()
+      .replace(/0+$/,"")}</span>€</p>
      </div>
 
      <div class="item__content__description">
@@ -55,46 +57,94 @@ const productDisplay = async () => {
                   <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
                 </div>
               </div>
-
+              
               <div class="item__content__addButton">
-                <button id="addToCart">Ajouter au panier</button>
+                <button id=${productData._id}>Ajouter au panier</button>
               </div>
-
+              
             </div>
           </article>
    `;
- 
-  
 
+   let select = document.getElementById("colors");
+   console.log(select);
 
+   console.log(productData.colors);
 
-
- let select = document.getElementById("colors")
- console.log(select)
-
- console.log(productData.colors)
-
- productData.colors.forEach(couleur => {
-  console.log(document.createElement("option"));
+   productData.colors.forEach((colors) => {
+   
   let tagOption = document.createElement("option");
-  tagOption.innerText = `${couleur}`;
-  tagOption.value = `${couleur}`;
-  
+
+  tagOption.innerHTML= `${colors}` ;
+  tagOption.value =`${colors}`;
+
   select.appendChild(tagOption);
- });
-console.log(colors)
+  console.log(tagOption)
+  });
 
-addCart(productData);
+  addBasket(productData);
 };
-
 productDisplay();
+  
+ 
+const addBasket = () => {
+console.log("bonjour");
+let bouton = document.getElementById(productData._id);
+console.log(bouton);
+bouton.addEventListener("click", () => {
+  let produitTableau = JSON.parse(localStorage.getItem("product"))
+  console.log(produitTableau)
+ let select = document.getElementById("colors")
+  console.log(select.value)
+  let quantite = document.getElementById("quantity")
+  console.log(quantite)
+  let nomProduit = document.getElementById("title")
+  console.log(nomProduit);
+  
+  const fusionproduitColor = Object.assign({}, productData, {
+    teinte:`${select.value}`,
+    quantite: 1,
+    nomProduit: `${productData.name}`
+  })
+console.log(fusionproduitColor);
+//document.location.href="./cart.html"
+ 
 
-const addCart = () => {
-    console.log("bonjour");
-    let bouton = document.getElementById(productData._id);
-    console.log(bouton)
-    bouton.addEventListener("click", () =>{
-        let select = document.getElementById("colors");
-        console.log(select);
-    });
-};
+//const popupConfirmation = () =>{
+  //if(window.confirm(`${nomProduit} option: ${select.value} a bien été ajouté au panier Consultez le panier OK ou revenir a l'accueil ANNULER`)){
+//window.location.href="cart.html";
+ // }else{
+
+ // }
+//}
+
+
+
+
+if(produitTableau){
+  produitTableau.push(productData);
+  localStorage.setItem("product", JSON.stringify(produitTableau))
+//popupConfirmation();
+}
+  else  {
+    produitTableau = [];
+    produitTableau.push(productData);
+    console.log(produitTableau);
+    localStorage.setItem("product", JSON.stringify(produitTableau))
+   //popupConfirmation();
+  }
+  
+})
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
